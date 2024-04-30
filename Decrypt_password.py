98d47e981b2b11b59705040587ad1f11e4fe5945
Niall Dcunha <152503679+Niall1985@users.noreply.github.com>
2024-04-30 14:50:32 +0530

    Update Decrypt_password.py

--- Decrypt_password.py
+++ Decrypt_password.py
@@ -8,7 +8,7 @@ def generate_passwords(min_length=1, max_length=20):
         yield from ("".join(password) for password in passwords)
 #the generate_passwords function generates passwords for testing using capital and small letters, numbers and special characters
 
-def crack_password(pdf_path, min_length, max_length, progress_interval=1):
+def crack_password(pdf_path, min_length, max_length, progress_interval=100):
     pdf_reader = PyPDF2.PdfReader(open(pdf_path, "rb"))
     attempt_count = 0
 
@@ -25,6 +25,8 @@ def crack_password(pdf_path, min_length, max_length, progress_interval=1):
             if pdf_reader.decrypt(password) == 1:
                 print(f"Password cracked: {password}")
                 return password
+                break
+                
     print("Password not found")
     return None
 
